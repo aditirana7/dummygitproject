8836258bde29a9c31e925750b8907b189aa8e0ac
Administrator <Administrator@172.16.51.3>
2024-08-12 21:39:21 -0700

    second commit

--- src/comm/demogit/Testdemogit.java
+++ src/comm/demogit/Testdemogit.java
@@ -4,7 +4,10 @@ public class Testdemogit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a=10,b=20;
+		int sum=a+b;
+		System.out.println(sum);
+		
 	}
 
 }
